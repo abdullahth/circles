b33e2c859505496a191a18f962b6e406a8476366
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-15 06:34:37 +0200

    constants

---
+++ circles/lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Colors
+final cPrimaryColor = Color(0xFF248501);
+final cSecondryColor = Color(0xFF629C06);
+final cAccentColor = Color(0xFF069C10);
+final cBottomNavigationActiveColor = cAccentColor;
+final cImageCircularBorderColor =
+    LinearGradient(colors: [cPrimaryColor, cSecondryColor]);
+final cTextColor = Color(0xFF464E53);
+
+final cDarkModePrimaryColor = cPrimaryColor;
+final cDarkModeSecondryColor = cSecondryColor;
+final cDarkModeBottomNavigationActiveColor = cBottomNavigationActiveColor;
+final cDarkModeImageCircularBorderColor = Colors.white;
+final cDarkModeAccentColorColor = cAccentColor;
+final cDarkModeTextColor = Colors.white;
+
+final defultPadding = 12.0;
