85c6e6c0ac73a63be75f811aae7ec99c1cfdc0c2
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-15 06:35:07 +0200

    main file

---
+++ circles/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:circles/constants.dart';
+import 'package:circles/screens/auth-screens/welcome-screen/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Circles',
+      theme: ThemeData(
+        primaryColor: cPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
