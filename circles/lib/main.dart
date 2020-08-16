227e7f76a38279698a3ebb3f0a10967f21dbd685
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-16 12:40:45 +0200

    main file updated

--- circles/lib/main.dart
+++ circles/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:circles/constants.dart';
+import 'package:circles/routes.dart';
 import 'package:circles/screens/auth-screens/welcome-screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: WelcomeScreen(),
+      routes: routes,
     );
   }
 }
