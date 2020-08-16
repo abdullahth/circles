e8a3e23a1c8e0eed3f512ed4b6e3e948e369c3c4
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-16 12:43:18 +0200

    Updated Welcome Screen Button

--- circles/lib/screens/auth-screens/welcome-screen/widgets/button.dart
+++ circles/lib/screens/auth-screens/welcome-screen/widgets/button.dart
@@ -1,8 +1,8 @@
+import 'package:circles/screens/auth-screens/auth-pages/components/signin-body.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../constants.dart';
 import '../../../../transitions.dart';
-import '../../auth-screen.dart';
 
 class WelcomeScreenButton extends StatelessWidget {
   const WelcomeScreenButton({
@@ -11,27 +11,33 @@ class WelcomeScreenButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-      onPressed: () {
-        Navigator.push(context, BouncyTransistion(widget: AuthScreen()));
-      },
-      color: cPrimaryColor,
-      splashColor: Colors.transparent,
-      child: Row(
-        children: [
-          Spacer(),
-          RichText(
-            text: TextSpan(
-                text: "Continuo!",
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900)),
-          ),
-          Spacer()
-        ],
-      ),
-    );
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+        child: SizedBox(
+            height: 50,
+            child: FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              onPressed: () {
+                Navigator.push(
+                    context, BouncyTransistion(widget: SignInBody()));
+              },
+              color: cPrimaryColor,
+              splashColor: Colors.transparent,
+              child: Row(
+                children: [
+                  Spacer(),
+                  RichText(
+                    text: TextSpan(
+                        text: "Continuo!",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900)),
+                  ),
+                  Spacer()
+                ],
+              ),
+            )));
   }
 }
