e2d6fcf3e52ff0a7a39438b2949d9d760dc74d07
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-19 10:24:38 +0200

    Updated App Routes

--- circles/lib/routes.dart
+++ circles/lib/routes.dart
@@ -1,11 +1,11 @@
 import 'package:circles/screens/auth-screens/auth-pages/signin-body.dart';
 import 'package:circles/screens/auth-screens/auth-pages/signup-body.dart';
-import 'package:circles/screens/auth-screens/auth-pages/signin-successful.dart';
+// import 'package:circles/screens/auth-screens/auth-pages/signin-successful.dart';
 import 'package:flutter/material.dart';
 
 // The Routes Here is only the ones to be used it Navigator.pushNamed();
 final Map<String, WidgetBuilder> routes = {
-  SignInSuccessful().routeName: (context) => SignInSuccessful(),
+  // SignInSuccessful().routeName: (context) => SignInSuccessful(),
   SignUpBody().routeName: (context) => SignUpBody(),
   SignInBody().routeName: (context) => SignInBody(),
 };
