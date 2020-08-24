8bb0cef455618941b321aef4cc92292734f615f5
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-24 19:46:37 +0200

    Welcome Screen

---
+++ circles/lib/screens/welcome-auth-screens/welcome-screen/components/body.dart
@@ -0,0 +1,88 @@
+import 'package:circles/config/dim-config.dart';
+import 'package:circles/screens/welcome-auth-screens/auth/signin/signin.dart';
+import 'package:flutter/material.dart';
+
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.black12, Colors.black87],
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter),
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/welcome-image.jpg"))),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 12, top: 32),
+                      child: RichText(
+                          text: TextSpan(
+                              text: "Circles",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 72,
+                              ))),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 12, top: 12),
+                      child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                              text: "Find Your Buddy!",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 24,
+                              ))),
+                    ),
+                  ]),
+            ),
+            Spacer(
+              flex: 5,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50), color: Colors.red),
+                child: SizedBox(
+                  height:
+                      proptionateHieght(36, MediaQuery.of(context).size.height),
+                  width: double.infinity,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, SignInPage.route);
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: "Continue!",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 24))),
+                  ),
+                ),
+              ),
+            ),
+            Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+}
