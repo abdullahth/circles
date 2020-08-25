5d0442491869b97b1efbdac0a39b87c7519febca
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-25 02:11:23 +0200

    Routes: Step 2

--- circles/lib/config/routes.dart
+++ circles/lib/config/routes.dart
@@ -1,6 +1,10 @@
+import 'package:circles/screens/home/home.dart';
 import 'package:circles/screens/welcome-auth-screens/auth/signin/signin.dart';
+import 'package:circles/screens/welcome-auth-screens/auth/signup/signup.dart';
 import 'package:flutter/cupertino.dart';
 
 Map<String, WidgetBuilder> routes = {
-  SignInPage.route: (context) => SignInPage()
+  SignInPage.route: (context) => SignInPage(),
+  SignUpPage.route: (context) => SignUpPage(),
+  HomePage.route: (context) => HomePage()
 };
