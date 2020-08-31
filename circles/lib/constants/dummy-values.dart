787f7ae6914f9243db36d75a63c7d0274ccc195c
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-31 02:12:41 +0200

    Dummy Values

---
+++ circles/lib/constants/dummy-values.dart
@@ -0,0 +1,6 @@
+import 'package:circles/models/circle-model.dart';
+
+List<Circle> dummyCircles = [
+  Circle("Circle Header One", "Circle Bio One", "Circle Rules",
+      "Circle Image Url", "Creater Uid", 6, 22, 0, DateTime.now(), null)
+];
