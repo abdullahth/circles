beabae69b9558ab02ef0d405273daf87cb24dc36
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-31 02:12:11 +0200

    Circles Details Screen init

---
+++ circles/lib/screens/details-screens/circle/circle-details.dart
@@ -0,0 +1,24 @@
+import 'package:circles/models/circle-model.dart';
+import 'package:flutter/material.dart';
+
+class CircleDetails extends StatefulWidget {
+  Circle circle;
+  static String route = "CIRCLE_DETAILS_ROUTE";
+  CircleDetails({Circle circle}) {
+    this.circle = circle;
+  }
+  @override
+  _CircleDetailsState createState() => _CircleDetailsState(this.circle);
+}
+
+class _CircleDetailsState extends State<CircleDetails> {
+  Circle circle;
+  _CircleDetailsState(Circle circle) {
+    this.circle = circle;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
