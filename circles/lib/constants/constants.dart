61ed1dbac0f76f05e5c4a726445492d8151dab72
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-08-31 02:12:56 +0200

    Constants

---
+++ circles/lib/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:circles/models/interest.dart';
+import 'package:flutter/material.dart';
+
+List<Interest> interests = [
+  Interest(
+      "Food",
+      "Meet People that are care about food and visiting restaurants, cafes and take away boses",
+      0,
+      Image.asset(
+        "assets/icons/food.png",
+        color: Colors.red,
+      ))
+];
